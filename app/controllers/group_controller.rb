2d39c2308fc629a40d7a4f796070c9066afae42e
dipsikha <dipsikha21@gmail.com>
2015-04-19 23:17:39 -0700

    Basic controller functionality

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,2 +1,23 @@
 class GroupController < ApplicationController
+
+	def create
+    	Group.create(group_params)
+	end
+
+	def group_params
+    	params.require(:group).permit(:name, :location)
+  	end
+
+	def join
+		@group = Group.find(params[:gid])
+		user = User.find(params[:uid])
+   		group.users <<  user #Add user to group
+    end
+
+
+	def destroy
+		@group.destroy
+		flash[:notice] = "This group has been deleted."
+	end
+
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,6 @@
 class Group < ActiveRecord::Base
 	belongs_to :location
+	has_and_belongs_to_many :users
+	validates :name, presence: true
+	validates :location, presence: true
 end
