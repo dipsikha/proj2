d8e7039e15f35b264109b38914deb9d5bb15e995
jennyfromtheweb <jennyzhcl@yahoo.com>
2015-04-23 18:22:27 -0700

    fixed seed file and model errors

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,2 @@
 class Group < ActiveRecord::Base
-	belongs_to :location
-	has_and_belongs_to_many :users
-	validates :name, presence: true
-	validates :location, presence: true
 end
--- db/migrate/20150424012118_create_groups.rb
+++ db/migrate/20150424012118_create_groups.rb
@@ -1,6 +1,10 @@
 class CreateGroups < ActiveRecord::Migration
   def change
     create_table :groups do |t|
+      t.string :name
+      t.string :description
+      t.string :time
+      t.string :location
 
       t.timestamps
     end
