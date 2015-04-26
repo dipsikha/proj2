48637629d06952ca53f0a5283dd01e4605ec3588
jennyfromtheweb <jennyzhcl@yahoo.com>
2015-04-25 19:33:42 -0700

    fixed locations  page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :locations
   # resources :locations
   get '/locations', to: 'location#index'
-  post '/locations/:id', to: 'location#show'
+  get '/locations/:id', to: 'location#show'
 
   get '/groups', to: 'group#index'
   post '/groups/:id', to: 'group#show'
