1077e658831fccccb4f2ad6a4655aa748d42f77c
jennyfromtheweb <jennyzhcl@yahoo.com>
2015-04-29 19:45:38 -0700

    idk

--- config/routes.rb
+++ config/routes.rb
@@ -11,14 +11,14 @@ Rails.application.routes.draw do
   # get '/locations', to: 'location#index'
   # get '/locations/:id', to: 'location#show'
 
-  get '/users', to: 'user#index'
-  post '/users/:id', to: 'user#show'
+  # get '/users', to: 'user#index'
+  # post '/users/:id', to: 'user#show'
 
   # post '/groups/new', to: 'group#new', as: 'new_group'
   # post '/groups/', to: 'group#create'
   # get '/groups', to: 'group#index'
   # get '/groups/:id', to: 'group#show'
 
-  patch 'join', to: 'user#join', as: 'join' #this route is a problem
+  patch '/join', to: 'users#join', as: 'join' #this route is a problem
 
 end
